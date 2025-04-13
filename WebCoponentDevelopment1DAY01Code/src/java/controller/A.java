0d98904ec1311aa1e3c9d3f544a9bdb14d7d65a2
Shayon5759 <163113315+Shayon5759@users.noreply.github.com>
2025-04-13 16:41:18 +0530

    Add files via upload

---
+++ WebCoponentDevelopment1DAY01Code/src/java/controller/A.java
@@ -0,0 +1,58 @@
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "A", urlPatterns = {"/A"})
+public class A extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.print("<!DOCTYPE html>");
+            out.print("<html>");
+            out.print("<head>");
+            out.print("<title>WCD 1</title>");
+            out.print("</head>");
+            out.print("<body>");
+            out.print("<form method='POST' action='B'>");
+            out.print("<button type='submit'>Send</button>");
+            out.print("</form>");
+            out.print("</body>");
+            out.print("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ WebCoponentDevelopment1DAY01Code/src/java/controller/B.java
@@ -0,0 +1,49 @@
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "B", urlPatterns = {"/B"})
+public class B extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+          
+            out.println("<h1>B Servlet</h1>");
+        } finally {
+            out.close();
+        }
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
